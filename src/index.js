87ef8a6e79a90c54697e248beabb211695963d30
sodam <damsoshin.dev@gmail.com>
2024-08-26 06:53:04 +0900

    feat: 포스트 목록 custom element 적용

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// document.querySelector("#app").innerHTML = `
-//   <div>토스 기술 블로그, 토스 테크</div>
-// `;
+import { PostView } from "./view/PostView.js";
+
+customElements.define("post-view", PostView);
