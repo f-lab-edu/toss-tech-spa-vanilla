172dc6981c4231a41e9f46baac5c2840667407f6
sodam-run-dev <157378788+sodam-run-dev@users.noreply.github.com>
2024-08-28 14:28:52 +0000

    chore: src 디렉토리를 기준으로 절대 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   root: "src",
   publicDir: "../public",
   build: {
     outDir: "../dist",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
